ee213a811fa2be1ff91ec1e570d867e695a3795b
loghman-moradi79 <loghman7979@gmail.com>
2025-01-05 17:02:16 +0330

    Update views.py
    add username and password

--- myblog/sabzwebapp/views.py
+++ myblog/sabzwebapp/views.py
@@ -239,11 +239,8 @@ def information_authors(request, author_id):
     }
     return render(request, 'forms/information.html', context)
 
-first = name = 'loghman'
-last_name = 'moradi'
-
-
-
+username = 'you'
+password = '1234'
 
 
 
